f42db9896ca66095a75489850a50cb265cca32b4
riccardogriffo <riccardo.griffo@mail.polimi.it>
2020-10-29 06:50:16 +0000

    Introduzione di AdvectionOperator

---
+++ src/operators/AdvectionOperator.cpp
@@ -0,0 +1,41 @@
+#include "AdvectionOperator.h"
+
+AdvectionOperator::AdvectionOperator(const FeSpace &fespace, double advectionCoeff): fespace_(fespace),
+                                                                                     advectionCoeff_(advectionCoeff) {
+    unsigned int maxDegree = fespace.getMaxDegree();
+    referenceElement_ = FeSpaceElement(maxDegree, maxDegree, std::make_pair(-1., 1.), std::make_pair(-1., 1.));
+    computeReferenceMatrix1D();
+}
+
+void AdvectionOperator::computeReferenceMatrix1D() {
+
+    std::shared_ptr<GradientHierarchicalBasis> basisPointer(
+            new GradientHierarchicalBasis(referenceElement_.getDegreeX()+1, referenceElement_.getDomainX())
+    );
+    IntegralMatrix1D matrix(basisPointer,basisPointer);
+    referenceMatrix1D_ = advectionCoeff_*matrix.generate(2*referenceElement_.getDegreeX());
+}
+
+Eigen::MatrixXd AdvectionOperator::computeLocalMatrix(FeSpaceElement feSpaceElement) {
+
+    Eigen::MatrixXd identityX = Eigen::MatrixXd::Identity(feSpaceElement.getDegreeX()+1, feSpaceElement.getDegreeX()+1);
+    Eigen::MatrixXd identityY = Eigen::MatrixXd::Identity(feSpaceElement.getDegreeY()+1, feSpaceElement.getDegreeY()+1);
+
+    Eigen::MatrixXd referenceX = referenceMatrix1D_.topLeftCorner(feSpaceElement.getDegreeX()+1,feSpaceElement.getDegreeX()+1);
+    Eigen::MatrixXd referenceY = referenceMatrix1D_.topLeftCorner(feSpaceElement.getDegreeY()+1,feSpaceElement.getDegreeY()+1);
+
+    Eigen::MatrixXd firstAddend = MatrixOperations::tensorProduct(4/(feSpaceElement.getLengthX()*feSpaceElement.getLengthX())*
+                                                                     identityY,referenceX);
+    Eigen::MatrixXd secondAddend = MatrixOperations::tensorProduct(4/(feSpaceElement.getLengthY()*feSpaceElement.getLengthY())*
+                                                                     referenceY,identityX);
+    return firstAddend + secondAddend;
+}
+
+void AdvectionOperator::updateSystemMatrix(SystemMatrix &systemMatrix) {
+    int curElemIdx = 0;
+    auto vector = fespace_.getFeSpaceElements();
+    for (auto &elem : vector) {
+        Eigen::MatrixXd localDiffusion = computeLocalMatrix(elem);
+        systemMatrix.addBlock(localDiffusion, curElemIdx, curElemIdx);
+        ++curElemIdx;
+    }
---
+++ src/operators/AdvectionOperator.h
@@ -0,0 +1,29 @@
+//
+// Created by Riccardo on 01/12/2017.
+//
+
+#ifndef PACS_PELI_CHIAPPA_DIFFUSIONOPERATOR_H
+#define PACS_PELI_CHIAPPA_DIFFUSIONOPERATOR_H
+
+#include "Operator.h"
+#include "../utilities/MatrixOperations.h"
+#include "../basis/GradientHierarchicalBasis.h"
+#include "IntegralMatrix1D.h"
+#include "../domain/FeSpace.h"
+#include "../problems/SystemMatrix.h"
+
+class AdvectionOperator: public Operator {
+private:
+    FeSpace fespace_;
+    double advectionCoeff_;
+public:
+
+    AdvectionOperator(const FeSpace &fespace, double advectionCoeff);
+    void computeReferenceMatrix1D() override;
+    Eigen::MatrixXd computeLocalMatrix(FeSpaceElement feSpaceElement) ;
+    void updateSystemMatrix(SystemMatrix &systemMatrix);
+
+};
+
+
+#endif //PACS_PELI_CHIAPPA_DIFFUSIONOPERATOR_H
